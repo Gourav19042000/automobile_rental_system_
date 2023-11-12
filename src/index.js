f04d6d11ecbe0a8292b6b1dafa4945ce1e117a42
ADENA VISHNU VARDHAN REDDY <107762926+vishnu-vardhan-reddy-adena@users.noreply.github.com>
2023-11-12 17:55:28 +0530

    router updated

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './App';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router = {AppRouter} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
